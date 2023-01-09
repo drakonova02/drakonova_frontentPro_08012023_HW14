06b34f4c588643edb62b0b0ef7bf864459667234
alesiad <drakonova02@gmail.com>
2023-01-09 22:52:56 +0100

    remark

--- HW14/index.js
+++ HW14/index.js
@@ -24,54 +24,69 @@ console.log(dollarToGrivnia.join('\n'));
 const n = 18;       // number N
 const arrQueue = [];
 
-if(n % 1) {
-    n.trunc();
-}
-
 // ОДЗ for sqrt
 
-if(n > 0) {
-    for(let i = 1; i <= 100 && i <= Math.sqrt(n); ++i) {
-        arrQueue.push(i);
+if(n % 1 === 0) {
+    if(n > 0) {
+        for(let i = 1; i <= 100 && i <= Math.sqrt(n); ++i) {
+            arrQueue.push(i);
+        }
+        
+        console.log(`Task 3: ${arrQueue.join(" ")}`);
     }
-    
-    console.log(`Task 3: ${arrQueue.join(" ")}`);
+} else {
+    console.log('Number isn`t whole')
 }
 
 // Task 4
 
-const number = 0;
+const number = 1;
 let flag = true;
 
-if(number % 1) {
-    number.trunc();
-}
+if(number % 1 === 0) {
+    if(number < 2) {
+        console.log(`Error: number should to be more 1`)
+    } else {
+        for(let i = 2; i < number; ++i) {
+            if(number % i === 0) {
+                flag = false;
+                break;
+            } 
+        }
 
-for(let i = 2; i < number; ++i) {
-    if(number % i === 0) {
-        flag = false;
-        break;
-    } 
+        flag ? console.log(`${number} is primary`) : console.log(`${number} isn't primary`);
+    }
+} else {
+    console.log('Number isn`t whole')
 }
-
-flag ? console.log(`${number} is primary`) : console.log(`${number} isn't primary`);
 
 // Task 5
 
-const number2 = 80;
+const number2 = 729;
 
-if(number2 % 1) {
-    number2.trunc();
-}
-
-if(number2 < 1 || number2 % 3 !== 0) {
-    console.log(`3^n !== ${number2}`);
-} else {
-    const poor = (Math.log(number2) / Math.log(3)).toFixed(2);
-
-    if(poor % 1) {
+if(number2 % 1 === 0) {
+    if (number2 === 1) {
+        console.log(`3^0 === ${number2}`);
+    } else if(number2 < 1 || number2 % 3 !== 0) {
         console.log(`3^n !== ${number2}`);
     } else {
-        console.log(`3^${poor} === ${number2}`);
+        let poor = 0;
+        let flagDiv = false;
+        
+        for (let i = 3; i <= number2; i *= 3) {
+            ++poor;
+            if(i === number2) {
+                flagDiv = true;
+                break;
+            }
+        }
+    
+        if(flagDiv) {
+            console.log(`3^${poor} === ${number2}`);
+        } else {
+            console.log(`3^n !== ${number2}`);
+        }
     }
+} else {
+    console.log('Number isn`t whole')
 }
